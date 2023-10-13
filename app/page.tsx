60ac0afa824a7e442f545f2d4d4aa10928a75070
Gladys Pascual <gkate@squarespace.com>
2023-10-13 17:23:09 +0100

    add dark mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
-import Image from 'next/image';
+import { ModeToggle } from '@/components/ModeToggle';
+import Onboarding from '@/components/Onboarding';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
-        <div>
-          {' '}
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </div>
-      </div>
+    <main className='px-24 py-16'>
+      <header className='flex justify-end'>
+        <ModeToggle />
+      </header>
+      <Onboarding />
     </main>
   );
 }
